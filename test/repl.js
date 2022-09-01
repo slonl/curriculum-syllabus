20d65471d1e65340a4b4770c2116c92be506e22a
aKacharava <a.kacharava94@gmail.com>
2022-09-01 16:44:09 +0200

    add repl module script

--- test/repl.js
+++
@@ -1,7 +0,0 @@
-const repl = require('repl');
-const curriculum = require('../curriculum-basis/lib/curriculum.js');
-curriculum.loadSchema('curriculum-basis/context.json','curriculum-basis/');
-curriculum.loadSchema('curriculum-examenprogramma/context.json','curriculum-examenprogramma/');
-curriculum.loadSchema('context.json');
-
-repl.start('> ').context.curriculum = curriculum;
